6d74096b3150e79e7ba523e642615ba481c97f8d
cloud <=>
2020-08-15 13:41:59 +0200

    run 1

--- frontend/src/components/UserCreate/UserCreate.vue
+++ frontend/src/components/UserCreate/UserCreate.vue
@@ -1,77 +1,25 @@
 <template>
   <v-form v-model="valid" v-if="!submitted">
     <v-container>
-      <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            prepend-icon="mdi-name"
-            v-model="firstname"
-            :rules="nameRules"
-            :counter="10"
-            label="First name"
-            required
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            prepend-icon="mdi-name"
-            v-model="lastname"
-            :rules="nameRules"
-            :counter="10"
-            label="Last name"
-            required
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            prepend-icon="mdi-email"
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="password"
-            prepend-icon="mdi-lock"
-            label="Password"
-            type="password"
-            required
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn
-              color="success"
-              class="mr-4"
-              @click="create"
-              ripple
-              rounded
-              block
-            >Create Account
-            </v-btn>
-          </v-card-actions>
-        </v-col>
-      </v-row>
+      <v-text-field
+        v-for="(item, key) in model"
+        :v-model=item
+        :key=key
+        :placeholder=item
+        :label=item
+      />
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="success"
+          class="mr-4"
+          @click="create"
+          ripple
+          rounded
+          block
+        >Create Account
+        </v-btn>
+      </v-card-actions>
     </v-container>
   </v-form>
   <div class="text-center" v-else>
@@ -104,42 +52,26 @@ export default {
   store,
   data: () => ({
     valid: false,
-    nameRules: [
-      v => !!v || 'Name is required',
-      v => v.length <= 10 || 'Name must be less than 10 characters',
-    ],
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+/.test(v) || 'E-mail must be valid',
-    ],
-    payload: {
-      firstname: '',
-      lastname: '',
-      password: '',
-      isadmin: '',
-      email: '',
+    submitted: false,
+    model: {
+      first_name: "First Name",
+      last_name: 'Last Name',
+      password: 'Password',
+      email: 'email',
     }
   }),
-  props: {
-    submitted: false
-  },
+  props: {},
   methods: {
     ...mapActions(["createUser"]),
     create() {
-      var payload = {
-        first_name: this.firstname,
-        last_name: this.lastname,
-        is_admin: this.isadmin,
-        email: this.email,
-        password: this.password
-      };
-      console.log(payload)
-      this.$store.dispatch("createUser", payload)
-      this.submitted = true;
+      let payload = this.model
+      console.log(this.model.email)
+      console.log(this.model.first_name)
+      // this.$store.dispatch("createUser", payload)
+      this.submitted = false;
     },
     newUser() {
       this.submitted = false;
-      this.payload = {};
     }
   }
 
