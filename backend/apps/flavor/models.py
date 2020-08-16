66f57e677b672c4387485e03b88f84d74d28a31b
cloud <=>
2020-08-16 22:52:51 +0200

    Pickled Fields

--- backend/apps/flavor/models.py
+++ backend/apps/flavor/models.py
@@ -1,11 +1,18 @@
 import uuid
 
 from django.db import models
-
+from picklefield.fields import PickledObjectField
 
 # Create your models here.
+class Spectrum(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(null=True, max_length=75)
+    data = PickledObjectField(copy=False)
 
 
 class Flavor(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(verbose_name="Flavor Name", max_length=75, default="flavor")
+    spectrum = models.ForeignKey(
+        Spectrum, related_name="flavor", on_delete=models.CASCADE, null=True
+    )
